263f6676918ffee243ddccffacb8dc3b136f08e8
Ivar Pruijn <ivar@pruijn.com>
2014-07-15 13:13:45 +0000

    Debugger code

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync();
+var index = fs.readFileSync("index.html");
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
+    var userAgent = req.headers["user-agent"];
+    if (userAgent) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(index);
+    }
 }).listen(9615);
